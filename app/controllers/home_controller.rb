eb5f283a6080b5c3c5547cde6ae07f349986f7c7
Noah Holmes <noah.r.holmes@gmail.com>
2014-11-21 15:27:06 -0500

    pulled from master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+	def search
+		parameters = { term: params[:term], limit: 16 }
+		render json: Yelp.client.search(‘Place Searched’, parameters)
+	end
 end
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+	config.consumer_key = ENV['YELP_KEY']
+	config.consumer_secret = ENV['YELP_SECRET']
+	config.token = ENV['YELP_TOKEN']
+	config.token_secret = ENV['YELP_TOKEN_SECRET']
+end

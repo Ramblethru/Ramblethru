19f837991fe2a4addb6d0567412a8de0027878a0
gregurton <gburt802@gmail.com>
2014-11-21 15:07:58 -0500

    twitter omniauth login is partway there

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :foursquare, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end

b6175946d012588406ec74912b265289143f487b
Noah Holmes <noah.r.holmes@gmail.com>
2014-11-20 23:42:13 -0500

    Ajax for yelp semi working - needs love

--- app/assets/javascripts/discovers.js
+++ app/assets/javascripts/discovers.js
@@ -1,10 +1,11 @@
 
 // foursquare api example call
 $.ajax({
-	type: 'POST',
+	type: 'GET',
 	dataType: "json",
-	url: 'https://api.foursquare.com/v2/venues/search?client_id=1BFAML3P0MNEHNINJ3GUHPRJXCY4ZM3JM14LZBGXEOAB4PFT&client_secret=E5R0UEZFE0JDJB2ON0K31RBAYOPZ3CHWKR3JFK0BDELV2CUJ&v=20130815&ll=40.7,-74&query=sushi',
+	url: 'https://api.foursquare.com/v2/venues/explore?client_id=1BFAML3P0MNEHNINJ3GUHPRJXCY4ZM3JM14LZBGXEOAB4PFT&client_secret=E5R0UEZFE0JDJB2ON0K31RBAYOPZ3CHWKR3JFK0BDELV2CUJ&v=20130815&ll=35.9,-78.9',
 	success: function(data) {
-		$('.foursq').append(data.response.venues[0].name);
+		$('.foursq').append('<div class="yelpapi">' + '<a href="' + data.response.groups[0].items[0].venue.url + '"' + '>' + data.response.groups[0].items[0].venue.name +
+		'</a>' + " - " + "rating " + data.response.groups[0].items[0].venue.rating);
 	}
 });

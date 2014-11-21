94167e35a4743b5a026b1699f2ab5077129d13db
gregurton <gburt802@gmail.com>
2014-11-20 20:52:19 -0500

    fixed users controller issue (extra end)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,40 @@
 class UsersController < ApplicationController
 
-    def show
-      @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def new
-      @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:success] = 'User Created'
-        redirect_to new_login_path
-      else
-        flash[:error] = 'There was a problem creating your account.'
-        render :new
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User Created'
+      redirect_to new_login_path
+    else
+      flash[:error] = 'There was a problem creating your account.'
+      render :new
     end
+  end
 
-    def update
-      @user.update(user_params)
-      redirect_to @user
-    end
+  def update
+    @user.update(user_params)
+    redirect_to @user
+  end
 
-    def destroy
-      @user.destroy
-      redirect_to users_url, notice: 'User was successfully destroyed.'
-      end
-    end
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.'
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :photo, :bio, :lives_in, :username, :password_digest)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :photo, :bio, :lives_in, :username, :password_digest)
+  end
 end

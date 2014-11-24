0ee8df4c780c691811283c1116ecb8c2ac0619f9
Noah Holmes <noah.r.holmes@gmail.com>
2014-11-24 16:02:17 -0500

    photo upload semi working

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  has_attached_file :photo, :styles => { :medium => "300x300>",
-                                         :thumb => "100x100>" }
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :authorizations
 
--- db/migrate/20141120042008_create_users.rb
+++ db/migrate/20141120042008_create_users.rb
@@ -3,12 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.attachment :photo
       t.text :bio
       t.string :lives_in
       t.string :username
       t.string :password_digest
-
       t.timestamps
     end
   end

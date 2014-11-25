3a25029c6a11e290dfa6850d6bb999b5b235acfd
gregurton <gburt802@gmail.com>
2014-11-25 11:54:02 -0500

    did some code cleanup in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,30 +2,24 @@ class User < ActiveRecord::Base
 
   has_many :rambles
   has_many :notes, through: :rambles
+  has_many :authorizations
 
   validates :name, presence: true
   validates :email, presence: true
-           
 
   has_secure_password
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment :avatar,
   :content_type => { :content_type => ["image/jpeg", "image/png"] }
-  
-  has_many :authorizations
 
   def self.find_or_create_by_auth_hash(auth_hash)
     auth = Authorization.find_or_create_by!(provider: auth_hash["provider"],
                                             uid: auth_hash["uid"])
     auth.update!(token: auth_hash["credentials"]["token"])
-
-
     unless auth.user
       auth.create_user!(email: auth_hash["info"]["email"],
                         name: auth_hash["info"]["name"])
-                        
-  
       auth.save!
     end
     auth.user

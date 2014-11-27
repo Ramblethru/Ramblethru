78bbcd668a60d6909fa916a8a07aea688e0fc93a
gregurton <gburt802@gmail.com>
2014-11-27 13:36:03 -0500

    adjusted rambles controller so that APIs could return responses based on destination user specified

--- app/controllers/rambles_controller.rb
+++ app/controllers/rambles_controller.rb
@@ -3,36 +3,37 @@ class RamblesController < ApplicationController
 
     before_action :authenticate
     # before_save { |ramble| ramble.destination.downcase! }
- def show
-        yelp             
-        instagram
-        reddit
-        foursquare
-        @ramble = Ramble.find(params[:id])
+  def show
+    yelp
+    instagram
+    reddit
+    foursquare
+    @ramble = Ramble.find(params[:id])
   end
 
   def yelp
-        @ramble = Ramble.find(params[:id])
-        params = { term: 'food',
-           limit: 5,
-       }
-       @yelp = Yelp.client.search('New York', params)
+    params = { term: 'food',
+               limit: 5,
+    }
+    @yelp = Yelp.client.search("#{@ramble.destination}", params)
   end
 
   def instagram
-    instagram = HTTParty.get('https://api.instagram.com/v1/media/search?lat=40.7&lng=74.0&count=8&client_id=ea93d7b97c444c9bbfcf23cbbcb63ee4')
+    instagram = HTTParty.get("https://api.instagram.com/v1/media/search?lat=#{@ramble.latitude}&lng=#{@ramble.longitude}&count=8&client_id=ea93d7b97c444c9bbfcf23cbbcb63ee4")
     instagram_data = JSON.parse(instagram.body)
     @instagram_images = instagram_data["data"]
+    # [0]["images"]["thumbnail"]["url"]
   end
 
   def reddit
-    reddit = HTTParty.get("http://www.reddit.com/r/subreddit/search.json?q=newyorkcity&limit=5&sort=top")   
+    uri = Addressable::URI.parse("http://www.reddit.com/r/subreddit/search.json?q=#{@ramble.destination}&limit=5&sort=top")
+    reddit = HTTParty.get(uri.normalize)
     reddit_data = JSON.parse(reddit.body)
     @reddit_thread = reddit_data["data"]["children"]
   end
 
   def foursquare
-    foursquare = HTTParty.get("http://api.foursquare.com/v2/venues/explore?near=NYC&limit=5&oauth_token=3SFP4NBFWJ2LIECDWGR5EU4FA5QXMP21LK2DNWT2GEUWCEIN&v=20141123")
+    foursquare = HTTParty.get("http://api.foursquare.com/v2/venues/explore?ll=#{@ramble.latitude},#{@ramble.longitude}&limit=5&oauth_token=3SFP4NBFWJ2LIECDWGR5EU4FA5QXMP21LK2DNWT2GEUWCEIN&v=20141123")
     foursquare_data = JSON.parse(foursquare.body)
     @foursquare_venue = foursquare_data["response"]["groups"][0]["items"]
     @foursquare_tip = foursquare_data["response"]["groups"][0]["items"]
@@ -56,7 +57,6 @@ class RamblesController < ApplicationController
         render :create, status: :not_found
       end
     end
-  end
   end
 
   def new
@@ -88,14 +88,13 @@ class RamblesController < ApplicationController
   end
 
   def update
-   @ramble = Ramble.find(params[:id])
-   if @ramble.update(ramble_params)
-    flash[:notice] = 'Ramble was successfully updated.' 
-    redirect_to @ramble
-
-  else
-    render :edit 
-  end
+    @ramble = Ramble.find(params[:id])
+    if @ramble.update(ramble_params)
+      flash[:notice] = 'Ramble was successfully updated.'
+      redirect_to @ramble
+    else
+      render :edit
+    end
   end
 
   def edit
@@ -107,7 +106,7 @@ class RamblesController < ApplicationController
   end
 
   def find_ramble
-  Ramble.find(:all, :conditions => conditions)
+    Ramble.find(:all, :conditions => conditions)
   end
 
 private
@@ -115,5 +114,4 @@ private
   def ramble_params
   params.require(:ramble).permit(:start_date, :end_date, :name, :destination, :user_id, :reddit_thread)
   end
-  
 end

b9724db8f56c63b2e11b443b86ae83c75332b34f
Danielle <danielleheckman5@gmail.com>
2014-11-26 20:22:03 -0500

    much changes to homepage styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,6 @@ Rails.application.config.assets.precompile += %w( normalize.css )
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery.usmap.js )
+Rails.application.config.assets.precompile += %w( raphael.js )
+Rails.application.config.assets.precompile += %w( animate.css )

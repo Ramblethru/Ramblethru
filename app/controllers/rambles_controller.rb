bea54cf5e4b993396dc416bf17311cbddb0129e7
gregurton <gburt802@gmail.com>
2014-11-27 22:37:31 -0500

    rambles pages now showing up OK -- needed to put ramble = ramble.find line above all the api calls in the controller

--- app/controllers/rambles_controller.rb
+++ app/controllers/rambles_controller.rb
@@ -29,6 +29,7 @@ class RamblesController < ApplicationController
   end
 
   def yelp
+    @ramble = Ramble.find(params[:id])
     params = { term: 'food',
                limit: 5,
     }
@@ -36,6 +37,7 @@ class RamblesController < ApplicationController
   end
 
   def instagram
+    @ramble = Ramble.find(params[:id])
     instagram = HTTParty.get("https://api.instagram.com/v1/media/search?lat=#{@ramble.latitude}&lng=#{@ramble.longitude}&count=8&client_id=ea93d7b97c444c9bbfcf23cbbcb63ee4")
     instagram_data = JSON.parse(instagram.body)
     @instagram_images = instagram_data["data"]
@@ -43,6 +45,7 @@ class RamblesController < ApplicationController
   end
 
   def reddit
+    @ramble = Ramble.find(params[:id])
     uri = Addressable::URI.parse("http://www.reddit.com/r/subreddit/search.json?q=#{@ramble.destination}&limit=5&sort=top")
     reddit = HTTParty.get(uri.normalize)
     reddit_data = JSON.parse(reddit.body)
@@ -50,6 +53,7 @@ class RamblesController < ApplicationController
   end
 
   def foursquare
+    @ramble = Ramble.find(params[:id])
     foursquare = HTTParty.get("http://api.foursquare.com/v2/venues/explore?ll=#{@ramble.latitude},#{@ramble.longitude}&limit=5&oauth_token=3SFP4NBFWJ2LIECDWGR5EU4FA5QXMP21LK2DNWT2GEUWCEIN&v=20141123")
     foursquare_data = JSON.parse(foursquare.body)
     @foursquare_venue = foursquare_data["response"]["groups"][0]["items"]

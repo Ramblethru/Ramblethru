0d0772b1c5ca49b68d84d98646d0348da5205064
gregurton <gburt802@gmail.com>
2014-11-30 14:10:22 -0500

    started working on  background styling for note tags--next step is to figure out how to render them separately so each tag is in its own box

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,66 +1,66 @@
 class NotesController < ApplicationController
  include Taggable
 
- def index
-  @notes = Note.all
-end
+  def index
+    @notes = Note.all
+  end
 
-def show
-  @note = Note.find(params[:id])
-end
+  def show
+    @note = Note.find(params[:id])
+  end
 
-def new
-  @note = Note.new
-end
+  def new
+    @note = Note.new
+  end
 
-def create
-  @ramble = Ramble.find(params[:ramble_id])
-  @note = @ramble.notes.create(note_params)
-  respond_to do |format|
-    format.html do
-      if @note.save
-        redirect_to @ramble, success: "Your note was recorded."
+  def create
+    @ramble = Ramble.find(params[:ramble_id])
+    @note = @ramble.notes.create(note_params)
+    respond_to do |format|
+      format.html do
+        if @note.save
+          redirect_to @ramble, success: "Your note was recorded."
+        end
       end
-    end
-    format.js do
-      if @note.save
-        render :create, status: :created
-      else
-        render :create, status: :not_found
+      format.js do
+        if @note.save
+          render :create, status: :created
+        else
+          render :create, status: :not_found
+        end
       end
     end
   end
-end
 
-def update
-  @ramble = Ramble.find(params[:ramble_id])
-  @note = @ramble.notes.find(params[:id])
-  @note.update(note_params)
-  redirect_to @ramble
-end
+  def update
+    @ramble = Ramble.find(params[:ramble_id])
+    @note = @ramble.notes.find(params[:id])
+    @note.update(note_params)
+    redirect_to @ramble
+  end
 
-def edit
-  @ramble = Ramble.find(params[:ramble_id])
-  @note = @ramble.notes.find(params[:id])
-end
+  def edit
+    @ramble = Ramble.find(params[:ramble_id])
+    @note = @ramble.notes.find(params[:id])
+  end
 
-def destroy
-  @note = Note.find(params[:id])
-  respond_to do |format|
-    format.html do
-      @note.destroy
-      redirect_to root_path
-    end
-    format.js do
-      @note.destroy
-      render 'notes/remove', status: :success
+  def destroy
+    @note = Note.find(params[:id])
+    respond_to do |format|
+      format.html do
+        @note.destroy
+        redirect_to root_path
+      end
+      format.js do
+        @note.destroy
+        render 'notes/remove', status: :success
+      end
     end
   end
-end
 
-private
+ private
 
-def note_params
-  params.require(:note).permit(:body, :user_id, :ramble_id, :tag_list)
-end
+  def note_params
+    params.require(:note).permit(:body, :user_id, :ramble_id, :tag_list)
+  end
 end

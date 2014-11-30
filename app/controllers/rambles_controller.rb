a6c5f7d3f7d862df02a306447725e620cd09cffd
Noah Holmes <noah.r.holmes@gmail.com>
2014-11-30 18:26:44 -0500

    minor changes

--- app/controllers/rambles_controller.rb
+++ app/controllers/rambles_controller.rb
@@ -2,7 +2,7 @@ require "addressable/uri"
 class RamblesController < ApplicationController
   include HTTParty
   before_action :authenticate
-  # before_save { |ramble| ramble.destination.downcase!
+  #before_save { |ramble| ramble.destination.downcase! }
 
     def show
        @ramble = Ramble.find(params[:id])

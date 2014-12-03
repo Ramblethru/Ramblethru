8ee8546e1dfd7c0f90756c0913a5dcbb9ceb8f2e
Noah Holmes <noah.r.holmes@gmail.com>
2014-12-03 16:20:59 -0500

    potentially fixed ramble show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,6 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
-  # GET /locations
-  # GET /locations.json
   def index
     if params[:search].present?
       @locations = Location.near(params[:search], 50, :order => :distance)
@@ -11,58 +9,45 @@ class LocationsController < ApplicationController
     end
   end
 
-  # GET /locations/1
-  # GET /locations/1.json
   def show
   end
 
-  # GET /locations/new
   def new
     @location = Location.new
   end
 
-  # GET /locations/1/edit
   def edit
   end
 
-  # POST /locations
-  # POST /locations.json
   def create
     @location = Location.new(location_params)
       if @location.save
         redirect_to @location
-        flash[:notice] = 'Location was successfully created.' 
+        flash[:notice] = 'Location was successfully created.'
       else
-        render :new 
+        render :new
       end
   end
 
-  # PATCH/PUT /locations/1
-  # PATCH/PUT /locations/1.json
   def update
     respond_to do |format|
       if @location.update(location_params)
         format.html { redirect_to @location, notice: 'Location was successfully updated.' }
-        format.json { render :show, status: :ok, location: @location }
       else
         format.html { render :edit }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /locations/1
-  # DELETE /locations/1.json
   def destroy
     @location.destroy
     respond_to do |format|
       format.html { redirect_to locations_url, notice: 'Location was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_location
       @location = Location.find(params[:id])
     end

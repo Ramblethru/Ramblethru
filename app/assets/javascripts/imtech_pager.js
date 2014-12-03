cceeccb1de4c63f88972b2edc49efe13a027e354
gregurton <gburt802@gmail.com>
2014-12-03 17:06:25 -0500

    commented out jquery.min.js for now (added it for jquery pagination) because having it present was breaking most if not all js features of the site. It may well be redundant or have a conflicting piece, not sure yet.

--- app/assets/javascripts/imtech_pager.js
+++ app/assets/javascripts/imtech_pager.js
@@ -4,16 +4,16 @@ Imtech.Pager = function() {
     this.currentPage = 1;
     this.pagingControlsContainer = "#pagingControls";
     this.pagingContainerPath = "#content";
-    
+
     this.numPages = function() {
         var numPages = 0;
         if (this.paragraphs != null && this.paragraphsPerPage != null) {
             numPages = Math.ceil(this.paragraphs.length / this.paragraphsPerPage);
         }
-        
+
         return numPages;
     };
-    
+
     this.showPage = function(page) {
         this.currentPage = page;
         var html = "";
@@ -23,12 +23,12 @@ Imtech.Pager = function() {
                 html += "<" + elem.tagName + ">" + elem.innerHTML + "</" + elem.tagName + ">";
             }
         }
-        
+
         $(this.pagingContainerPath).html(html);
-        
+
         renderControls(this.pagingControlsContainer, this.currentPage, this.numPages());
     }
-    
+
     var renderControls = function(container, currentPage, numPages) {
         var pagingControls = "Page: <ul>";
         for (var i = 1; i <= numPages; i++) {
@@ -38,9 +38,9 @@ Imtech.Pager = function() {
                 pagingControls += "<li>" + i + "</li>";
             }
         }
-        
+
         pagingControls += "</ul>";
-        
+
         $(container).html(pagingControls);
     }
 }

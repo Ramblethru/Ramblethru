493acfd72b283831bd72bae8d294dd599125366b
Noah Holmes <noah.r.holmes@gmail.com>
2014-12-07 00:54:33 -0500

    working on user profile not showing edit/other links unless logged in as that user

--- app/models/ramble.rb
+++ app/models/ramble.rb
@@ -12,6 +12,7 @@ class Ramble < ActiveRecord::Base
 
   before_save { |ramble| ramble.destination.downcase! }
 
+
   def self.search(query)
     #where(:destination, query)
     where("destination like ?", "%#{query.downcase}%")

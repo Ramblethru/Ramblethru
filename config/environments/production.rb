0b6c436af710c82733c891e9f3a62b8a85fc57b2
Danielle <danielleheckman5@gmail.com>
2014-12-06 20:51:06 -0500

    M

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
+  config.assets.compress = true
+
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 

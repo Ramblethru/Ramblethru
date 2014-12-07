63e9040baf5b8d0e245a60fd88f8ff1fe26275d0
Danielle <danielleheckman5@gmail.com>
2014-12-06 21:50:56 -0500

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-
-  config.assets.compress = true
-
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,10 +74,5 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-
-
-
-  # Do not dump schema after migrations.
-
-
+  config.active_record.dump_schema_after_migration = false
 end

aa8576251bfd971a7c2bf3f8c2ef47aa3989fb14
Noah Holmes <noah.r.holmes@gmail.com>
2014-12-08 16:25:38 -0500

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,14 @@ Rails.application.configure do
 
 
   # Do not dump schema after migrations.
-
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 
 end

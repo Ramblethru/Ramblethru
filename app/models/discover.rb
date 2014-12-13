0eb2176dbb9297e71d66581793f06500253648aa
gregurton <gburt802@gmail.com>
2014-12-12 22:36:48 -0500

    code cleanup to models

--- app/models/discover.rb
+++ app/models/discover.rb
@@ -2,10 +2,8 @@ class Discover < ActiveRecord::Base
   include FriendlyId
 
   friendly_id :destination, use: :slugged
-
   geocoded_by :destination
   after_validation :geocode, :if => :destination_changed?
-
   before_save { |discover| discover.destination.downcase! }
 
 end

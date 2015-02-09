b895390bff3be144f0e4d1cfd4e8148d566f535d
gregurton <gburt802@gmail.com>
2015-02-09 18:02:56 -0500

    updated application.rb to serve static assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Ramblethru
   class Application < Rails::Application
+
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
